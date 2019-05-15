a3d9f4633a23b43de181448bf0b8048385d29e0e
Loubeants Celestin <loubeants@gmail.com>
2019-05-15 17:29:00 -0400

    modal

---
+++ public/forms.js
@@ -0,0 +1,27 @@
+// modal 
+ $(document).ready(function () {
+    $('.modal').modal()
+    modalPop=()=>{
+        $('.modal').modal('open')
+    }
+    setTimeout(modalPop,3000)
+});
+    
+    // parent form click function
+    $("#modal1").click(function(){
+        $("#parent").toggle();
+        $(".modal-content").hide();
+        $("#sitterOrParent").hide();
+        $("#sitter").hide();
+        $("#mainPage").hide();
+        $("footer").hide();
+    });
+    // sitter form click function
+    $("#form2").click(function(){
+        $("#sitter").toggle();
+        $("#modal1").hide();
+        $("#sitterOrParent").hide();
+        $("#parent").hide();
+        $("#mainPage").hide();
+        $("footer").hide();
+    });
