a1a000ba69386c9d4f3ed89082a2f7048f7bb930
Justin Summers <jsummers07@gmail.com>
2019-05-15 13:36:16 -0400

    completed htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,36 @@
-var db = require("../models");
+//var db = require("../models");
+var path = require("path");
 
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/sithub.html"));
+  });
+  // Load Parent page
+  app.get("/parent", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/parent.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load Sitter Intake
+  app.get("/sitter", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/sitter.html"));
   });
+
+  // Load Sitter Results
+  app.get("/result", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/result.html"));
+  });
+
+  //app.get("/example/:id", function(req, res) {
+  //  db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //    res.render("example", {
+  //      example: dbExample
+  //    });
+  //  });
+  // });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  app.get("/404", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/404.html"));
   });
 };
