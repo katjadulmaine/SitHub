9c6a583e602a0185032b5080bb52732154e9422d
Lara Eller <lareh_eller@yahoo.com>
2019-05-16 12:55:47 -0400

    date dropdown

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -23,9 +23,7 @@ module.exports = function(app) {
   });
 
   app.get("/sitter/date", function(req, res) {
-    res.render("date", {
-      msg: "Welcome!"
-    });
+    res.render("date", { layout: "bootstrap" });
   });
 
   // Render 404 page for any unmatched routes
